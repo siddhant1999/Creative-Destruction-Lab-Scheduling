d767b86e24feedfcf436832f893217a0564ef20e
Siddhant Jain <siddhant99.jain@hotmail.com>
2017-01-29 18:49:33 -0500

    Just testing

--- processQuery.php
+++ processQuery.php
@@ -4,28 +4,68 @@ $username = "cl44-siddhant";
 $password = "123456";
 $dbname = "cl44-siddhant";
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Create connection
 
+// Check if the connection is good
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-//echo "process.php";
 
-$src1= $_POST['sqlQuery'];
-echo "Hi";
-echo $scr1;
+//Execute the query and store it in $result
 
-//echo mysqli_query($conn,$scr1);
+//$src1= $_POST['sqlQuery'];
+$src1 = "SELECT * FROM Meetings WHERE Venture_Name='ICSPI' ORDER BY Time_Start;" ;
+$result = $conn->query($src1);
 
-foreach ($src1 as &$str) {
-	echo $conn->query($str);
-	/*$foo =$conn->query($str);
-	if ($foo === TRUE) {
-		echo $foo;
-    	echo "New record created successfully";
-	} else {
-    	echo "Error: " . $sql . "<br>" . $conn->error;
-	}*/
+
+$fullArray = array(array());
+
+// there are 3 queries that I have to make
+//
+
+while ($row = $result->fetch_array()){
+    $row1 = array();
+
+    $row1["Meeting_Number"] = $row['Meeting_Number'];
+    $row1["Lead_1"] = $row['Lead_1'];
+    $row1["Lead_2"] = $row['Lead_2'];
+    $row1["Lead_3"] = $row['Lead_3'];
+    $row1["Time_Start"] = $row['Time_Start'];
+    $row1["Time_End"] = $row['Time_End'];
+    $row1["Room_Number"] = $row['Room_Number'];
+    
+    //array_push($fullArray, $row1);
+    break;
+    //we don't need the venture name because we query by it
+
+    /*
+        $row is the current entry
+
+        Use $row['FieldName'] to get information of a column
+        For example, $row['Meeting_ID'] returns the Meeting ID
+    */
+
+    //echo $row['Meeting_ID'];
 }
+
+//echo $fullArray;
+/*foreach($fullArray as &$k){
+    foreach($k as &$o){
+        echo $o;
+        echo " ";
+    }
+    echo "<br>";
+}
+*/
+
+$abc = json_encode($fullArray);
+echo var_dump($abc);
+echo "hello";
+echo $abc;
+//echo var_dump($fullArray);
+//echo 
+?>
