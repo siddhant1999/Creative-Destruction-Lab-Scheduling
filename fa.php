209b8ee89b5abb7cdb5789df3f873cfd5282e5d7
Siddhant Jain <siddhant99.jain@hotmail.com>
2017-03-07 14:13:59 -0500

    Additions

--- fa.php
+++ fa.php
@@ -87,10 +87,11 @@ else :
 //This is where we will process all requests
 
 var name = "<?php echo $leadName; ?>";
-alert(name);
+var date = "<?php echo $curDate; ?>";
 
+// we still need to implement a method of toggling between dates
 
-var query = "SELECT * FROM Meetings WHERE LOWER(Lead_1)='" + name +"' OR LOWER(Lead_2)='" + name +"' OR LOWER(Lead_3)='" + name +"' OR Meeting_Number IS NULL ORDER BY Time_Start;" ;
+var query = "SELECT * FROM Meetings WHERE Date='"+ date +"' AND (LOWER(Lead_1)='" + name +"' OR LOWER(Lead_2)='" + name +"' OR LOWER(Lead_3)='" + name +"' OR Meeting_Number IS NULL) ORDER BY Time_Start;" ;
 
 executeQuery(query);
 	
@@ -105,20 +106,184 @@ function executeQuery(query){
 		        },
 		        success: function( data ) {
 		        	console.log("Here is the data retrived:");
-		        	document.write(data);
+		        	console.log(data);
 
-		            //console.log(data);
-		            //console.log(allstrings);
-		            //alert("Complete");
 		        }
 		    });
 }
 
+function process(obj){
+
+	var distable = "<table class='mytable'><thead><tr><th style='min-width: 9.2em;'>Time</th><th>Venture</th><th>Discussion Leads</th></tr></thead><tbody>";
+	var prev1 = "";
+	var prev2 = "";
+
+	var idk = true;
+	for (var i = 0; i < obj.length; i++) {
+		var arr = obj[i];
+		distable += "<tr class='row_" + i + "'>";
+		if (arr['is_Custom']) {
+			var ty = har;
+			if (har == "A&amp;K Robotics") {ty = "A&K Robotics"}
+
+				if (arr['Venture_Name']!=ty) {
+					continue;
+				}
+		}
+
+		var a = helper(arr["Time_Start"]);
+		var b = helper(arr["Time_End"]);
+
+		if (i==0 || idk) {
+			idk = false;
+			var tempa = arr["Time_Start"];
+			subtracting =true;
+			tempa = helper(tempa);
+			subtracting = false;
+
+			distable += "<td style='background-color: #ececec; text-align:center;'>" + tempa + " - " + a +"</td><td style='background-color: #ececec;' colspan='2'>Check In at CDL Office <b>(Room 2052)</b></td></tr>" + "<tr>";
+
+		}
+
+		//k so now that we have the times this is a good time to put in the breaks
+		//just check if the end time doesnt match up with the next start time, and if not add a break
+
+
+
+		if (prev1!=a && prev2 !=b) {
+
+			if (prev2 != a && prev2) {
+				//add break
+				//console.log("Need break here");
+				distable += "<td style='background-color: #ececec; text-align:center;'>" + prev2 + " - " + a +"</td><td style='background-color: #ececec;' colspan='2'>Break</td></tr>" + "<tr class='row_" + i + "'>";
+			}
+
+			prev1 = a;
+			prev2 = b;
+			var k = i+1;
+			var count =1;
+
+			if (k < obj.length) {
+				while(a == helper(obj[k]["Time_Start"]) && b== helper(obj[k]["Time_End"])){
+					count++;
+					k++;
+					if (k == obj.length) {
+						break;
+					}
+				}
+			}distable += "<td style='text-align:center;' rowspan='"+ count +"'>" + a + " - " + b +"</td>";
+		}
+
+
+		if (arr["Meeting_Number"]) {
+			//meaning this is a specific individual meeting
+
+			distable += "<td colspan='2'>Individual Meeting with " + arr["Lead_1"];
+			for (var l = 2;l<4; l++) {
+				var tt = "Lead_" + l;
+				if (arr[tt]) {
+					distable += " and "  + arr[tt];
+				}
+				else break;
+
+			}
+
+			distable += " in <b>Room " + arr["Room_Number"] + "</b></td>";
+		}
+		else if (arr["Description"]) {
+			if (arr['is_Custom']) {
+				//console.log("isCustom");
+				//console.log(har + " + " + arr['Venture_Name']);
+				var ty = har;
+				if (har == "A&amp;K Robotics") {ty = "A&K Robotics"}
+				if (arr['Venture_Name']==ty) {
+					distable += "<td colspan='2'>" + arr["Description"] + "</td>";
+					//console.log("Yay");
+				}
+			}
+			else {
+				//this is a general activity to all
+				distable += "<td colspan='2'>" + arr["Description"] + "</td>";
+				isGrey.push(i);
+			}
+		}
+		else if(arr["Venture_Name"] && arr["Lead_1"]) {
+			//this is a track meeting
+			if (arr["Venture_Name"] == har) {
+				distable += "<td><b>" + arr["Venture_Name"] + "<b></td><td><b>" + arr["Lead_1"] + "</b></td>";
+			}
+			else distable += "<td>" + arr["Venture_Name"] + "</td><td>" + arr["Lead_1"] + "</td>";
+		}
+		
+		/*for (var key in obj){
+    		var value = obj[key];
+			//we may not necessarily need to iterate over all o
+    		//document.write(" " + key + ": " + value);
+  		}*/
+  		distable += "</tr>";
+  		//document.write("<br> ");
+	}
+	distable += "</tbody></table>";
+
+	$("#scheduleTable").append("<h3><b><u>" + har + "</u><i> - " + queryDate +"</i></b></h3>");
+	$("#scheduleTable").append(distable);
+	for (var j = 0; j < isGrey.length; j++) {
+		var theid = ".row_" + isGrey[j];
+
+		$(theid).css("background-color", "#ececec");
+	}
+
+	$("#scheduleTable").append("<br><h4><b><u>REMINDER: </b></u> Please arrive at each of your meeting locations 10 minute early.</h4>");
+
+	var ut, pw;
+
+	if (har[0] <= 'B') {
+		ut = "qq190481";
+		pw = "Uve2eothaa";
+	}
+	else if (har[0] == 'C' || (har[0] == 'D' && har[0] == 'a')) {
+		ut = "qq190482";
+		pw = "eu2Kapeisi"
+	}
+	else if (har[0] < 'F') {
+		ut = "qq190483";
+		pw = "jo5Uqueima";
+	}
+	else if (har[0] <= 'G') {
+		ut = "qq190484";
+		pw = "ahT5reegee";
+	}
+	else if (har[0] <= 'K') {
+		ut = "qq190469";
+		pw = "ieCe8pheek";
+	}
+	else if (har[0] <= 'N') {
+		ut = "qq190476";
+		pw = "quiexai7It";
+	}
+	else if (har[0] <= 'R') {
+		ut = "qq190479";
+		pw = "phiaLira7r";
+	}	
+	else if (har[0] <= 'T') {
+		ut = "qq190480";
+		pw = "eth2oeLees";
+	}
+	else {
+		ut = "qq190485";
+		pw = "Jix5aiquai";
+	}
+	$("#scheduleTable").append("<h4>Network: <b>UofT</b> | Username: <b>"+ ut + "</b> | Password: <b>" + pw + "</b></h4>");
+	$("#scheduleTable").append("<h4><a href='https://goo.gl/maps/8uSykS526Q22'>105 St George St, Toronto, ON M5S 2E8</a></h4><br>");
+	$("#venture").remove();
+	$("#subven").remove();
+}
 </script>
 
 <?php
 endif;
 ?>
+
 <script>
 // we also need the ability for the FAs to toggle between days (right now I will hard code that, but in the future, we really should be querying that from database, and by that I mean that we see what are the differences that appear in the database)
 
