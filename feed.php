9b926c9338103fb27d4bd00503e588c9205c1901
siddhant1999 <siddhant99.jain@hotmail.com>
2017-04-04 16:11:50 -0400

    no longer using outdated google rss feed

--- feed.php
+++ feed.php
@@ -1,12 +1,19 @@
 <html>
   <head>
-    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <!--
+        === Remove google's feed script ===
+        <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    -->
+
+    <!-- === Add this script === -->
+    <script type="text/javascript" src="https://rss2json.com/gfapi.js"></script>
+
     <script type="text/javascript">
 
     google.load("feeds", "1");
 
     function initialize() {
-      var feed = new google.feeds.Feed("http://fastpshb.appspot.com/feed/1/fastpshb");
+      var feed = new google.feeds.Feed("https://news.ycombinator.com/rss");
       feed.load(function(result) {
         if (!result.error) {
           var container = document.getElementById("feed");
